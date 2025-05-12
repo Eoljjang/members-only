15c60ea439bda33ec49e97b6054973e2b0d0ccdd
Nathan <nathan.wong2002@gmail.com>
2025-05-12 15:48:38 -0600

    updated pool

--- Pool.js
+++ Pool.js
@@ -3,10 +3,23 @@ require("dotenv").config();
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 // A "pool" is a collectaion of clients - it holds onto those connections.
-module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
-  user: process.env.POOL_USER,
-  database: "members_only",
-  password: "<role_password>",
-  port: parseInt(process.env.DB_PORT, 10) // The default port of postgresql.
-});
+let pool;
+
+// Running on Render or other hosted service
+if (process.env.DB_URL) {
+  pool = new Pool({
+    connectionString: process.env.DB_URL,
+    ssl: {
+      rejectUnauthorized: false // Required on Render
+    }
+  });
+} else {
+  // Local development config
+  pool = new Pool({
+    host: "localhost",
+    user: process.env.POOL_USER,
+    database: "members_only",
+    password: process.env.POOL_PASSWORD,
+    port: parseInt(process.env.DB_PORT, 10)
+  });
+}
